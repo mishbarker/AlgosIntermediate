a4a112d3cbfae3d45760acf14178664f04b07a2b
Michelle Barker <barkerwebdev47@gmail.com>
2020-09-07 19:29:50 -0700

    Test file

--- test.js
+++ test.js
@@ -1,33 +1,18 @@
 function swap(arr) {
     var temp = arr[0];
-    arr[0] = arr[arr.length-1];
-    arr[arr.length-1] = temp;
+    arr[0] = arr[arr.length - 1];
+    arr[arr.length - 1] = temp;
     return arr;
 }
-console.log(swap([1,5,10,-2]));
+console.log(swap([1, 5, 10, -2]));
 
-function sum_even(){
+function sum_even() {
     var sum = 0;
-    //your code here
-    for (var num = 6; num <= 17; num++){
-        if (num%2 ===0){
+    for (var num = 6; num <= 17; num++) {
+        if (num % 2 === 0) {
             sum = sum + num;
         }
     }
-    return sum; 
- }
- console.log(sum_even());
-
- var msg = 'dojo';
- if(msg == 'dojo') {
-     console.log('coding'+msg);
- }
- else
- {
-     console.log('ninja'+msg);
- }
-
- for(var x=17; x > 5; x--) {
-     console.log(x);
-     break;
- }
+    return sum;
+}
+console.log(sum_even());
